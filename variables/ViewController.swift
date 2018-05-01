c2debf86fe97078e11eeee01b7a2796d9ecd1d80
Chemistry Interactive <mbp24chem@chemistry-interactive.local>
2018-05-01 14:52:15 -0400

    First lesson

--- variables/ViewController.swift
+++ variables/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
+enum modes {
+    case notSet
+    case addition
+    case subtraction
+    case multiplcaiton
+}
+
 class ViewController: UIViewController {
 
+    var name:String = "Clint"
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var mode:modes = .addition
+
+        
+        print("Name: \(name)")
+        print(name)
+        let score:Int = 10
+        
+        myLabel.text = "Name: \(name) Score: \(score)"
+        //myLabel.textColor
     }
 
     override func didReceiveMemoryWarning() {
